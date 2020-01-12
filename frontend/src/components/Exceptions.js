1e3800b564add4a07b53219ce3a09b9f1719fef9
jglu225 <jglu225@uky.edu>
2020-01-12 18:19:46 -0500

    Added inc & dec function for numParticipants

---
+++ frontend/src/components/Exceptions.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Exceptions extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    
+    render(){
+
+        return(<div>Exceptions</div>)
+    }
+}
+
+export default Exceptions
--- frontend/src/components/Participants.js
+++ frontend/src/components/Participants.js
@@ -5,22 +5,38 @@ import "./styles/Participants.css";
 class Participants extends React.Component {
     constructor(props){
         super(props);
+        this.state = {
+            numParticipants: 6,
+        }
+
+        this.incParticipants = this.incParticipants.bind(this);
+        this.decParticipants = this.decParticipants.bind(this);
+    }
+
+    incParticipants(){
+        if(this.state.numParticipants < 16)
+            return this.setState({numParticipants: this.state.numParticipants+2});
+    }
+
+    decParticipants(){
+        if(this.state.numParticipants > 6)
+            return this.setState({numParticipants: this.state.numParticipants-2});
     }
 
     render() {
 
 
-      return(
+        return(
 
-        <div>   
-            <div className="numParticipants">
-                <div><Button size="lg">-</Button></div>
-                <div className="numParticipants_number">6</div>
-                <div><Button size="lg">+</Button></div>
-                <div className="numParticipants_label"><h3>Number of Participants</h3></div>
+            <div>   
+                <div className="numParticipants">
+                    <div><Button onClick={this.decParticipants} size="lg">-</Button></div>
+                    <div className="numParticipants_number">{this.state.numParticipants}</div>
+                    <div><Button onClick={this.incParticipants} size="lg">+</Button></div>
+                    <div className="numParticipants_label"><h3>Number of Participants</h3></div>
+                </div>
+
             </div>
-
-        </div>
         
       )
     }
